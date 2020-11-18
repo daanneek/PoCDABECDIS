c8e437af8493217fedb9c32a269407de9b3bf63a
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 13:32:43 +0100

    Fixed tray icon, message (i) showcase

--- ServerClient/TrayIconDemo.java
+++ ServerClient/TrayIconDemo.java
@@ -1,30 +1,24 @@
 package ServerClient;
 
 import java.awt.*;
-import java.awt.TrayIcon.MessageType;
 
 public class TrayIconDemo {
 
     public static void main(String[] args) throws AWTException {
-        if (SystemTray.isSupported()) {
-            TrayIconDemo td = new TrayIconDemo();
-            td.displayTray();
-        } else {
-            System.err.println("System tray not supported!");
-        }
+        sendNotification("Chart downloader", "New charts are done downloading!", "Fred.png");
     }
 
-    public void displayTray() throws AWTException {
-        // Obtain only one instance of the SystemTray object
-        SystemTray tray = SystemTray.getSystemTray();
-        // If the icon is a file
-        Image image = Toolkit.getDefaultToolkit().getImage("images/Fred.png");
-        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-        //Let the system resize the image if needed
-        trayIcon.setImageAutoSize(true);
-        //Set tooltip text for the tray icon
-        trayIcon.setToolTip("System tray icon demo");
-        tray.add(trayIcon);
-        trayIcon.displayMessage("ECDIS Chart Download", "Charts have finished downloading", MessageType.INFO);
+    public static void sendNotification(String title, String subtitle, String pathToIcon) {
+        SystemTray mainTray = SystemTray.getSystemTray();
+        Image trayIconImage = Toolkit.getDefaultToolkit().getImage(pathToIcon);
+        TrayIcon mainTrayIcon = new TrayIcon(trayIconImage);
+        mainTrayIcon.setImageAutoSize(true);
+        try {
+            mainTray.add(mainTrayIcon);
+            mainTrayIcon.displayMessage(title,  subtitle, TrayIcon.MessageType.NONE);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
