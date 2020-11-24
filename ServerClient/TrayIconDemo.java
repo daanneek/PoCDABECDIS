48dafbfb8e90cff685e447c755981d28541774a5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 12:06:27 +0100

    Added .png support for tray icon, working as intended TODO mac/linux support if needed, integration within receiver?

--- ServerClient/TrayIconDemo.java
+++ ServerClient/TrayIconDemo.java
@@ -1,24 +1,33 @@
 package ServerClient;
 
 import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+import java.io.IOException;
 
 public class TrayIconDemo {
 
     public static void main(String[] args) throws AWTException {
-        sendNotification("Chart downloader", "New charts are done downloading!", "Fred.png");
+        if (SystemTray.isSupported()) {
+            try{
+                TrayIconDemo td = new TrayIconDemo();
+                td.displayTray();
+            }
+            catch(Exception e){
+                System.err.println(e);
+            }
+        } else {
+            System.err.println("System tray not supported!");
+        }
     }
+    public void displayTray() throws AWTException, IOException {
+        SystemTray tray = SystemTray.getSystemTray();
 
-    public static void sendNotification(String title, String subtitle, String pathToIcon) {
-        SystemTray mainTray = SystemTray.getSystemTray();
-        Image trayIconImage = Toolkit.getDefaultToolkit().getImage(pathToIcon);
-        TrayIcon mainTrayIcon = new TrayIcon(trayIconImage);
-        mainTrayIcon.setImageAutoSize(true);
-        try {
-            mainTray.add(mainTrayIcon);
-            mainTrayIcon.displayMessage(title,  subtitle, TrayIcon.MessageType.NONE);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("images/rijkswaterstaat-squarelogo-1426746034306.png"));
+        TrayIcon trayIcon = new TrayIcon(image, "Chart downloader");
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setToolTip("Chart downloader");
+        tray.add(trayIcon);
+        trayIcon.displayMessage("Chart downloader", "All charts have been updated", MessageType.NONE);
+    
     }
 }
